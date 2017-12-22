fcf5de7434a1b0360cc53ccb73ae0dead83cf53d
huangzhijian <huangzhijian@xiaoyouzi.com>
2017-12-22 17:56:39 +0800

    add takeScreenshot

--- takeScreen.js
+++ takeScreen.js
@@ -15,13 +15,31 @@ function test() {
     });
     driver.takeScreenshot().then(function (bs64) {
         console.log(bs64);
+        console.log("data:image/png;base64,");
         //var dataBuffer = new Buffer(bs64, 'base64');
     });
     
     console.log("here");
 }
 
-test();
+function test2() {
+    driver.get('https://www.baidu.com/')
+    .then(function () {
+        driver.wait(until.elementLocated(By.id('head')), 30000);
+        return driver.findElement(By.id('head')).getAttribute('id');
+    })
+    .then(function (headID) {
+        console.log(headID);
+        return driver.takeScreenshot();
+    })
+    .then(function (bs64) {
+        console.log(bs64);
+        console.log("data:image/png;base64,");
+    });
+}
+
+
+test2();
 //var result = test();
 //console.log(result);
 console.log(123456);
