179538feaa046ac6c721f37687c038e8675c9a09
a316523235 <316523235@qq.com>
2017-12-26 23:40:37 +0800

    find product

--- tbk.js
+++ tbk.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var Promise = require('promise');
 
 var webdriver = require('selenium-webdriver'),
     By = webdriver.By,
@@ -50,63 +51,95 @@ function getTbkInfo2(shortUrl) {
 	})
 }
 
-function getTbkInfo(shortUrl) {
-	driver.get('https://www.alimama.com/index.htm');
-	driver.wait(until.elementLocated(By.id('J_menu_login')), 30000);
-	driver.findElement(By.id('J_menu_login')).click();
-	driver.wait(until.elementLocated(By.css('iframe[name=taobaoLoginIfr]')), 30000);
-	var loginIframe = driver.findElement(By.css('iframe[name=taobaoLoginIfr]'));
-	loginIframe.getAttribute('src').then(function(value) {
-		driver.get(value);
-		driver.executeScript('return $("#magix_vf_root").attr("id")').then(function(result) {
-			console.log("come");
-			console.log(result);
-		})
+function getTbkInfo3(shortUrl) {
+	// driver.get(shortUrl)
+	// .then(function() {
+	// 	driver.wait(until.urlContains('item.htm'), 30000);
+	// 	return driver.getCurrentUrl();
+	// })
+	// .then(function(realry) {
+
+	// })
+	var _realyUrl = "";
 
 
-		driver.wait(until.urlIs('https://www.alimama.com/index.htm'), 30000);
-		driver.get('https://pub.alimama.com/');
-		driver.wait(until.elementLocated(By.id('q')), 30000);
-		driver.findElement(By.id('q')).sendKeys(shortUrl);
-		driver.wait(until.elementLocated(By.id('qwertrtesttest')), 30000);
-
-
-
-
-
-		// //driver.wait(until.urlIs(value), 30000);
-		// driver.wait(until.elementLocated(By.css('J_QRCodeImg')), 90000);
-		// var jQRCodeImg = driver.findElement(By.css('J_QRCodeImg'));
-		// jQRCodeImg.getAttribute('class').then(function(testValue) {
-		// 	console.log(testValue);
-		// 	var jQRCodeImgImg = jQRCodeImg.findElement(By.tagName(img));
-		// 	jQRCodeImgImg.getAttribute('src').then(function(qrCodeUrl) {
-		// 		console.log('come here');
-		// 		console.log(qrCodeUrl);
-		// 		driver.wait(until.urlIs('https://www.alimama.com/index.htm'), 30000);
-		// 		driver.get('https://pub.alimama.com/');
-		// 		driver.wait(until.elementLocated(By.id('q')), 30000);
-		// 		driver.findElement(By.id('q')).sendKeys(shortUrl);
-		// 		driver.wait(until.elementLocated(By.id('qwertrtesttest')), 30000);
-		// 	})
-		// });
+	driver.get('https://pub.alimama.com/index.htm')
+	.then(function() {
+		var menuUserName = driver.wait(until.elementLocated(By.className('menu-username')), 30000);
+		return menuUserName.getText();
+	})
+	.then(function(userName) {
+		console.log('返回的名字：' + userName); 
+		if(userName.indexOf('冰风沉醉') > -1) {
+			return pOK();
+		} else {
+			return login();
+		}
+	})
+	.then(function() {
+		return driver.get(shortUrl);
+	})
+	.then(function() {
+		driver.wait(until.urlContains('item.htm'), 30000);
+		return driver.getCurrentUrl();
+	})
+	.then(function(realyUrl) {
+		console.log(realyUrl);
+		_realyUrl = realyUrl;
+		return driver.get('https://pub.alimama.com/index.htm');
+	})
+	.then(function() {
+		var q = driver.wait(until.elementLocated(By.id('q')), 30000);
+		console.log(_realyUrl);
+		return q.sendKeys(_realyUrl);
+	})
+	.then(function() {
+		return driver.findElement(By.className('search-btn')).click();
+	})
+	.then(function() {
+		driver.wait(until.elementLocated(By.id('q333333')), 30000);
 	})
 }
 
 function login() {
-	driver.get('https://pub.alimama.com/index.htm');
-	driver.wait(until.elementLocated(By.css('iframe[name=taobaoLoginIfr]')), 30000);
-
-
-
-	driver.executeScript('return $("#magix_vf_root").attr("id")').then(function(result) {
-		console.log("come");
-		console.log(result);
+	return new Promise(function(resolve, reject) {
+		driver.get('https://pub.alimama.com/index.htm')
+		.then(function() {
+			console.log(1);
+			var iframe = driver.wait(until.elementLocated(By.css('iframe[name=taobaoLoginIfr]')), 30000);
+			return iframe.getAttribute("src");
+		})
+		.then(function(loginIframeSrc) {
+			console.log(2);
+			return driver.get(loginIframeSrc);
+		})
+		.then(function() {
+			console.log(3);
+			var JQRCodeLogin = driver.wait(until.elementLocated(By.id('J_QRCodeLogin')), 10000);
+			var jQRCodeImg = JQRCodeLogin.findElement(By.id('J_QRCodeImg')).findElement(By.tagName('img'));
+			return jQRCodeImg.getAttribute('src');
+		})
+		.then(function(qrCodeUrl) {
+			console.log(4);
+			console.log('here');
+			console.log(qrCodeUrl);
+			alimamaDingTalk(qrCodeUrl);
+			driver.wait(until.urlContains('pub.alimama.com'), 60000);
+		})
+		.then(function() {
+			console.log(5);
+			resolve();
+		})
 	})
-	console.log('here');
-	//console.log(qrCodeUrl);
-	driver.wait(until.elementLocated(By.css('J_QRCodeImg')), 90000);
 }
+
+function pOK() {
+	return new Promise(function(resolve, reject) {
+		resolve();
+	})
+}
+
+
 
 function alimamaDingTalk(qrcode) {
 	console.log(qrcode);
@@ -139,5 +172,6 @@ function alimamaDingTalk(qrcode) {
 
 //example1();
 //example2();
-getTbkInfo2('https://detail.tmall.com/item.htm?id=556227443376&ut_sk=1.WdcRwYYfoP8DAEyjsHitmqkx_21380790_1513865805666.TaoPassword-Weixin.baobeixiangqingfenxiang&from=tbkfenxiangplus&suid=C9882239-5455-4881-A830-F1C1AC11E59B&tbkShareId=2555017770&systype=m&ali_trackid=2:mm_26632441_0_0:1513866407388_213_6540357480&fromScene=100&sourceType=item&shareid=CE9EA75B-45AB-435E-8833-0E04F035A8B8&un=aca25f20cf947f9246e6040d6f65052e&share_crt_v=1&cpp=1&shareurl=true&spm=a313p.22.4n.85329150789&short_name=h.BxEUKv&app=chrome');
+//getTbkInfo2('https://detail.tmall.com/item.htm?id=556227443376&ut_sk=1.WdcRwYYfoP8DAEyjsHitmqkx_21380790_1513865805666.TaoPassword-Weixin.baobeixiangqingfenxiang&from=tbkfenxiangplus&suid=C9882239-5455-4881-A830-F1C1AC11E59B&tbkShareId=2555017770&systype=m&ali_trackid=2:mm_26632441_0_0:1513866407388_213_6540357480&fromScene=100&sourceType=item&shareid=CE9EA75B-45AB-435E-8833-0E04F035A8B8&un=aca25f20cf947f9246e6040d6f65052e&share_crt_v=1&cpp=1&shareurl=true&spm=a313p.22.4n.85329150789&short_name=h.BxEUKv&app=chrome');
+getTbkInfo3('http://m.tb.cn/h.BxTYnx');
 //driver.quit();
